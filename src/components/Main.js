58136966004e1e1d08817ec4d0c60ae3754d7fff
Nesia <helena.maslowska17@gmail.com>
2023-02-28 00:53:18 +0100

    remove files and clear project to blank

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main()
+{
+	return (
+		<>
+		<p> Art Store </p>
+		</>
+		
+
+	)
+}
+export default Main;
---
+++ src/components/Margin.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Margin()
+{
+	return (
+		<div style={marginStyle}>
+			<h1> Welcome to OUR page!</h1>
+      		<p>Weeeeeeee</p>
+		</div>
+	)
+}
+
+const marginStyle = {
+    fontSize: '12px',
+    padding: '0rem',
+	color: 'black',
+}
