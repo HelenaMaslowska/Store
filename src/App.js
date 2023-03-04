6e0b247ef8fa8dc083ff9054bc6cfe56930c03b4
Łukasz <lukasz13102001@o2.pl>
2023-03-04 01:10:56 +0100

    Ideas of components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Main from './components/Main';
 import Margin from './components/Margin';
+import Menu from './components/Menu';
+import Header from './components/Header'
+import {BrowserRouter, Routes} from 'react-router-dom'
 
 export default function App() {
   return (
-    <>
-	<Margin/>
-	<Main/>
-	</>
+
+  <BrowserRouter>
+    
+      <Menu/>
+      <Header/>
+      <Margin/>
+      <Main/>
+
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+    </Routes>
+  </BrowserRouter>
+
   );
 }
 
